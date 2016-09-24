203ac887949238d1e8856ed59ad08ef4b01a460f
Atsushi Hayashida <rinrin0108.sp1@gmail.com>
2016-09-24 18:54:00 +0900

    added timer

---
+++ SuperNOVA-app/SuperNOVA-app/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  SuperNOVA-app
+//
+//  Created by y-okada on 2016/09/24.
+//  Copyright © 2016年 SuperNOVA. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import ObjectMapper
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
